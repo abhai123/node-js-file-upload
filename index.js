6c69a31b02c7236f59ccca4bbcd3c751d76b974f
abhai123 <92018693+abhai123@users.noreply.github.com>
2022-11-01 12:04:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+ 
+// default options
+app.use(fileUpload());
+ 
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+ 
+app.listen(3000)
